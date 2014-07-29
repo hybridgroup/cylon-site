4d9786c467dd58e5c38be8aee1db89e8b8991521
nathan <nathanzankich@gmail.com>
2014-07-29 10:43:38 -0700

    parallax background fix

--- source/javascripts/parallax_header.js
+++ source/javascripts/parallax_header.js
@@ -1,6 +1,5 @@
-$(document).ready(function(){
-        $('.parallax-top-left').plaxify({"xRange":20,"yRange":20});
-        $('.parallax-top-right').plaxify({"xRange":40,"yRange":40});
-        $('.parallax-top-back').plaxify({"xRange":10,"yRange":0});
+$(document).ready(function(){ 
+        $('.parallax-top').plaxify({"xRange":40,"yRange":40}); 
         $.plax.enable();
 });
+ 
