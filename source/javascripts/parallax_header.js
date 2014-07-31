3a38d8e1a3910b1ba1839f31d8bba66813004224
Luis Felipe Perez <slashcat@gmail.com>
2014-07-31 11:35:08 -0500

    js spaces

--- source/javascripts/parallax_header.js
+++ source/javascripts/parallax_header.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){ 
-    $('.parallax-top').plaxify({"xRange":10,"yRange":10}); 
+    $('.parallax-top').plaxify({"xRange":10, "yRange":10}); 
     $.plax.enable();
 });
  
