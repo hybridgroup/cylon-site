8e187bd1ed6c32eb81ac1848e1fe0c5af0955a0a
nathan <nathanzankich@gmail.com>
2014-10-20 09:46:37 -0700

    restucture hybridpages layouts and javascripts

--- source/javascripts/subnav.js
+++ source/javascripts/subnav.js
@@ -4,12 +4,10 @@ $(document).ready(function() {
     $(window).bind('scroll', function() {
       if ($(window).scrollTop() > navpos.top) {
         $('#mainsubnav').addClass('subNavfixed');
-        $('#HowToConnect, #HowToUse, #Commands, #Events, #Circuit, #Compatability,#SpheroColors,#Drivers,#HowToInstall').addClass('marginTopSubnav');
        }
        else {
-         $('#mainsubnav').removeClass('subNavfixed');
-         $('#HowToConnect, #HowToUse, #Commands, #Events, #Circuit, #Compatability,#SpheroColors,#Drivers,#HowToInstall').removeClass('marginTopSubnav');
-         $("#mainsubnav a").removeClass("active");
+        $('#mainsubnav').removeClass('subNavfixed');
+        $("#mainsubnav a").removeClass("active");
        }
     });
 
